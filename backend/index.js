58882f1fc983fd880481095d9bc5be63b1966299
supahkenneh <chunghlken@gmail.com>
2022-08-31 19:58:27 -0400

    init backend

---
+++ backend/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 8888;
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+})
+
+app.listen(PORT, () => {
+    console.log(`Server started on port: ${PORT}`);
+})
