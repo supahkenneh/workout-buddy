a06a16f6c46a949341065fe36eb35e4d2bb37af7
supahkenneh <chunghlken@gmail.com>
2022-09-01 14:16:02 -0400

    create sql query

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,15 @@
+require('dotenv').config();
+const bodyParser = require("body-parser");
 const express = require("express");
 const app = express();
+const { getUserLogs } = require('./queries');
 
 const PORT = process.env.PORT || 8888;
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
-})
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get('/', getUserLogs)
 
 app.listen(PORT, () => {
     console.log(`Server started on port: ${PORT}`);
